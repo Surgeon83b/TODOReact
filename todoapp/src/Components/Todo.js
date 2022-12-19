5708e1ac264407078f47cf6519e9223eccdaa9f4
Kanstantsin Piatkevich <surgeon-2@tut.by>
2022-12-19 14:49:56 +0300

    fix: bugs

--- todoapp/src/Components/Todo.js
+++ todoapp/src/Components/Todo.js
@@ -43,6 +43,7 @@ export function Todo() {
         break;
       default: setListOfTodos(completedTodos);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [shown]);
 
   useEffect(() => {
